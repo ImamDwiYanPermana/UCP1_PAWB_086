0b4382a7fc140d292cc0b1a7ea0e4949be2e3ecb
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-23 15:04:43 +0700

    memanggil localhost yang sudah tersediakan

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () =>
+  console.log(`server berjalan di port : http://localhost:${port}`)
+);
