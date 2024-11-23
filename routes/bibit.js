375065d1536e79e1e59c27f943f45277f8764044
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-23 14:52:24 +0700

    membuat isi database bibit

---
+++ routes/bibit.js
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+const router = express.Router();
+
+const bibit = [
+  {
+    nama_bibit: "padi",
+    varietas: "IR64",
+    kondisi_tumbuh: "Kebutuhan sinar matahari",
+  },
+];
