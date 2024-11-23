fa84e7d1fd0a000530a21e955e96a2f67a358d5b
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-23 15:01:32 +0700

    menambahkan port untuk local host dan app use untuk di gunakan

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pupukRoutes from "./routes/pupuk.js";
 import bibitRoutes from "./routes/bibit.js";
+
+const app = express();
+const port = 3000;
+
+app.use("/pupuk", pupukRoutes);
+app.use("/bibit", bibitRoutes);
