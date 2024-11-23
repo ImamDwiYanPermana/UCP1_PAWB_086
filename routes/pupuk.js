28a1af29aed66088158cc371ec172f444be70146
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-23 14:54:25 +0700

    membuat variable pupuk yang berisi atribut pupuk (database nya )

---
+++ routes/pupuk.js
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+const router = express.Router();
+
+const pupuk = [
+  {
+    nama_pupuk: "urea",
+    nutrisi: "nitrogen",
+    tipe_pupuk: "organik",
+  },
+];
