0d77b942ac17fd0bddefc531f1171316e408e3f4
ImamDwiYanPermana <imampermana244@gmail.com>
2024-11-23 14:57:27 +0700

    memanggil todos

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const todoRoutes = require("./routes/todo.js");
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+app.use("/todos", todoRoutes);
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
